ceb8f341d467b9bb33277b0883977631c4824c85
chris <pupu86019@gmail.com>
2024-11-04 16:25:14 +0800

    chore: remove placeholder workshop content

--- Counter.js
+++ Counter.js
@@ -4,72 +4,14 @@ class Counter extends HTMLElement {
     super();
     this.value = 0;
 
-    /* TO DO */ 
+    /* TO DO */
     // 創建 Shadow Root，封裝樣式和結構
-    this.attachShadow({ mode: 'open' });
 
     /* TO DO */
     // 定義template
-    const template = document.createElement('template');
-    template.innerHTML = `
-      <style>
-        :host {
-          display: block;
-          max-width: 150px;
-          background-color: white;
-          border-radius: 4px;
-          padding: 16px;
-          border: 1px solid #dddddd;
-          user-select: none;
-          text-align: center;
-        }
-        .value {
-          font-size: 48px;
-          font-family: sans-serif;
-          margin: 16px 0;
-        }
-        .buttons {
-          display: flex;
-          gap: 10px;
-          justify-content: center;
-        }
-        button {
-          flex-grow: 1;
-          font-size: 24px;
-          padding: 8px;
-          background: #dddddd;
-          color: #333333;
-          border: none;
-          border-radius: 4px;
-          cursor: pointer;
-        }
-        button:active {
-          background: #cccccc;
-        }
-      </style>
-
-      <div class="value">${this.value}</div>
-      <div class="buttons">
-        <button type="button" class="button--increment">
-          <slot name="increment"></slot>
-        </button>
-        <button type="button" class="button--decrement">
-          <slot name="decrement"></slot>
-        </button>
-      </div>
-    `;
 
     /* TO DO */
     // 將模板內容clone後插入到 Shadow DOM
-    this.shadowRoot.appendChild(template.content.cloneNode(true));
-
-    this.shadowRoot
-      .querySelector('.button--increment')
-      .addEventListener('click', this.increment.bind(this));
-
-    this.shadowRoot
-      .querySelector('.button--decrement')
-      .addEventListener('click', this.decrement.bind(this));
   }
 
   increment() {
@@ -87,7 +29,5 @@ class Counter extends HTMLElement {
   }
 }
 
-
-/* TO DO */ 
+/* TO DO */
 //註冊自定義的HTML element
-customElements.define('my-counter', Counter);
